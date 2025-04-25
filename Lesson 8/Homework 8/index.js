41ace2d6cbd6e851d5939e6b960db781dfe39355
Artur Somov <artursomov@yahoo.de>
2025-04-25 21:35:36 +0200

    Homework 8: JavaScript Basics. Variables.

---
+++ Lesson 8/Homework 8/index.js
@@ -0,0 +1,44 @@
+//1. Числовое значение
+
+let height = 170;
+console.log(height);
+
+let weight;
+weight = 90;
+console.log(weight);
+
+//Не уверен, можно ли таким образом создавать переменную
+const pi = 3.14159;
+console.log(pi);
+
+let temperature = -45.8;
+console.log(temperature);
+
+
+
+//2. Строка
+
+let surname = "Schmidt";
+console.log(surname);
+
+let bird;
+bird = "Chicken";
+console.log(bird);
+
+//Просто пример, не уверен используется ли const в реальной практике
+const capitalOfRussia = "Moscow";
+console.log(capitalOfRussia);
+
+
+
+//3. Булевое значение
+
+let isOnline = "false";
+console.log(isOnline);
+
+let isRegistered = "true";
+console.log(isRegistered);
+
+const isMember = "true";
+//isMember = "false"; Const нельзя переопределить
+console.log(isMember);
